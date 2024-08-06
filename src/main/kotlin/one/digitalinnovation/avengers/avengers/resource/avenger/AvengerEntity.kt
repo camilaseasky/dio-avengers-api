b7317ecdb23a4645e45222f3279d6174189e9db0
camila.barbosa <camila.barbosa@totemti.com.br>
2024-08-06 09:38:52 -0400

    feat: implementação camada de infra

---
+++ src/main/kotlin/one/digitalinnovation/avengers/avengers/resource/avenger/AvengerEntity.kt
@@ -0,0 +1,34 @@
+package one.digitalinnovation.avengers.avengers.resource.avenger
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import one.digitalinnovation.avengers.avengers.domain.avenger.Avenger
+
+@Entity
+data class AvengerEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(nullable = false)
+    val nick: String,
+    @Column(nullable = false)
+    val person: String,
+    val description: String?,
+    val history: String?
+) {
+    fun toAvenger() = Avenger(id, nick, person, description, history)
+
+    companion object {
+        fun from(avenger: Avenger) = AvengerEntity(
+            id = avenger.id,
+            nick = avenger.nick,
+            description = avenger.description,
+            history = avenger.history,
+            person = avenger.person
+        )
+    }
+
+}
---
+++ src/main/kotlin/one/digitalinnovation/avengers/avengers/resource/avenger/AvengerEntityRepository.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.avengers.avengers.resource.avenger
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AvengerEntityRepository : JpaRepository<AvengerEntity, Long> {
+
+}
